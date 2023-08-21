cb26c379489b0b785efd98cb31ce42bb409df28d
Oidaho <ruslan30403@gmail.com>
2023-08-21 18:06:28 +1000

    Хандлеры включены в оборот

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,15 @@
 from . import (
     ban_handler,
     mute_handler,
-    warn_handler
+    warn_handler,
+    queue_handler,
+    warnoverflow_handler
 )
 
-handlers = [ban_handler.Handler(), mute_handler.Handler(), warn_handler.Handler()]
+handlers = [
+    ban_handler.Handler(),
+    mute_handler.Handler(),
+    warn_handler.Handler(),
+    queue_handler.Handler(),
+    warnoverflow_handler.Handler()
+]
