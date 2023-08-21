19044b101f85e6e2676339ce3e9cd35a25f75a1c
Oidaho <ruslan30403@gmail.com>
2023-08-21 16:40:37 +1000

    Выдача мута при 3х варнах

---
+++ handlers/warnoverflow_handler.py
@@ -0,0 +1,34 @@
+import time
+
+from config import TIME_COEFFICENT, GROUP_URL, STUFF_ADMIN_ID
+from .abc_handler import ABCHandler
+
+
+class Handler(ABCHandler):
+    async def __send_respond(self, data):
+        title = f"@id{data.get('target_id')} (Пользователь) был заглушен.\n" \
+                f"Причина: {data.get('reason')}" \
+                f"Время снятия заглушения: {data.get('target_time')}\n" \
+                f"По вопросам обращаться к @id{STUFF_ADMIN_ID} (Администратору)."
+        await self.bot.api.messages.send(chat_id=data.get('peer_id') - 2000000000, message=title, random_id=0)
+
+    async def check(self):
+        overflow = self.database.get_overflow_warn()
+        if overflow:
+            for warn in overflow:
+                self.database.remove_warn(peer_id=warn[0], user_id=warn[1], force=True)
+                delta = TIME_COEFFICENT["h"]
+                all_data = await self.about.get_all_info(
+                    cpid=warn[0],
+                    ctid=warn[1],
+                    delta=delta,
+                    rsn="Получено три предупреждения"
+                )
+                all_data["initiator_id"] = 0
+                all_data["initiator_name"] = "Система"
+                all_data["initiator_url"] = GROUP_URL
+
+                self.database.add_mute(all_data)
+
+                await self.__send_respond(all_data)
+                await self._send_log(peer_id=warn[0], user_id=warn[1], command="mute")
