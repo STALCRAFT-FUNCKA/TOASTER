6366e54b540647a4db1d5269837c04148fad4265
Oidaho <ruslan30403@gmail.com>
2023-08-22 20:53:54 +1000

    Обработчик очереди

--- routes/queue.py
+++ routes/queue.py
@@ -1,10 +1,11 @@
 from vkbottle.bot import Bot, BotLabeler, Message
-from config import TOKEN, GROUP_ID
-from database.interface import Connection
-from logger.logger import Logger
+from config import TOKEN, GROUP_ID, GROUP_URL, STUFF_ADMIN_ID
+from database.sql_interface import Connection
+from utils.chat_logger import Logger
 from rules.custom_rules import IgnorePermission, HandleIn
 from utils.information_getter import About
 from utils.time_converter import Converter
+
 
 bot = Bot(token=TOKEN)
 bl = BotLabeler()
@@ -20,6 +21,35 @@ converter = Converter()
     blocking=False
 )
 async def queue(message: Message):
+    async def send_log(data, command, reason):
+        # формируем лог
+        logger.compose_log_data(
+            initiator_name=data.get("initiator_name"),
+            initiator_role=data.get("initiator_role"),
+            peer_name=data.get("peer_name"),
+            command_name=command,
+            reason=reason,
+            target_name=data.get("target_name_tagged"),
+            target_warns=data.get("target_warns"),
+            now_time=data.get("now_time"),
+            target_time=data.get("target_time")
+        )
+        logger.compose_log_attachments(
+            peer_id=data.get("peer_id"),
+            cmids=data.get("cmids")
+        )
+
+        # отправляем лог
+        await logger.log()
+
+    async def send_respond(data):
+        title = f"@id{data.get('target_id')} (Пользователь) получил предупреждение.\n" \
+                f"Причина: {data.get('reason')}.\n" \
+                f"Текущее количество предупреждений: {data.get('target_warns')}/3.\n" \
+                f"Время снятия предупреждений: {data.get('target_time')}\n" \
+                f"По вопросам обращаться к @id{STUFF_ADMIN_ID} (Администратору)."
+        await message.answer(title)
+
     async def collapse(m: Message):
         await bot.api.messages.delete(
             group_id=GROUP_ID,
@@ -32,17 +62,31 @@ async def queue(message: Message):
         return
 
     else:
-        all_data = await about.get_all_info(message)
-
-    if database.get_queue(peer_id=all_data.get("peer_id"), user_id=all_data.get("initiator_id")):
         reason = "Нарушение задержки"
-        ...
-        # warn
+        delta = converter.delta(0, "d")
+        all_data = await about.get_all_info(
+            cpid=message.peer_id,
+            ctid=message.from_id,
+            rsn=reason,
+            time_delta=delta
+        )
+        all_data["initiator_id"] = 0
+        all_data["initiator_name"] = "Система"
+        all_data["initiator_url"] = GROUP_URL
+        all_data["chat_id"] = message.peer_id - 2000000000
+        all_data["target_warns"] += 1
+        all_data["cmids"] = [message.conversation_message_id]
+
+    if database.get_queue(peer_id=all_data.get("peer_id"), user_id=all_data.get("target_id")):
+        database.add_warn(all_data)
+
+        await send_respond(all_data)
+        await send_log(all_data, command="warn", reason=reason)
 
         await collapse(message)
 
     else:
+        all_data["initiator_id"] = all_data["target_id"]
+        all_data["initiator_name"] = all_data["target_name"]
+        all_data["initiator_url"] = all_data["target_url"]
         database.add_queue(all_data)
-
-
-
