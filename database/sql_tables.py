7c52b9483018a73836e5e6b40a4e4ef89a9bb792
Oidaho <ruslan30403@gmail.com>
2023-09-01 23:34:37 +1000

    Убраны лишние столбцы. Унифицированы названия столбцов.

--- database/sql_tables.py
+++ database/sql_tables.py
@@ -1,102 +1,85 @@
 conversations = """CREATE TABLE IF NOT EXISTS conversations
                (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   peer_id INTEGER UNIQUE ON CONFLICT REPLACE,
+                   peer_id INTEGER PRIMARY KEY UNIQUE ON CONFLICT REPLACE,
                    peer_name TEXT,
-                   destination TEXT
+                   peer_type TEXT
                );"""
 
-# TODO: Сделать унифицированные названия столбцов
 permissions = """CREATE TABLE IF NOT EXISTS permissions
                 (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    user_id INTEGER, 
-                    user_name TEXT,
-                    user_url TEXT,
-                    permission_lvl INTEGER,
-                    permission_name TEXT,
-                    CONSTRAINT someone UNIQUE (user_id, peer_id) ON CONFLICT REPLACE
+                    peer_id INTEGER PRIMARY KEY REFERENCES conversations (peer_id) ON DELETE CASCADE,
+                    target_id INTEGER, 
+                    target_name TEXT,
+                    target_lvl INTEGER,
+                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT REPLACE
                 );"""
 
 settings = """CREATE TABLE IF NOT EXISTS settings
                 (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    setting_name TEXT UNIQUE ON CONFLICT REPLACE,
-                    setting_status INTEGER
+                    peer_id INTEGER PRIMARY KEY REFERENCES conversations (peer_id) ON DELETE CASCADE,
+                    setting_name TEXT,
+                    setting_status INTEGER,
+                    CONSTRAINT someone UNIQUE (peer_id, setting_name) ON CONFLICT REPLACE
                 );"""
 
 kicked = """CREATE TABLE IF NOT EXISTS kicked
                 (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    user_id INTEGER,
-                    user_name TEXT,
-                    user_url TEXT,
-                    kicked_by_id INTEGER,
-                    kicked_by_name TEXT,
-                    kicked_by_url TEXT,
+                    peer_id INTEGER PRIMARY KEY REFERENCES conversations (peer_id) ON DELETE CASCADE,
+                    initiator_id INTEGER,
+                    initiator_name TEXT,
+                    target_id INTEGER,
+                    target_name TEXT,
                     kick_time INTEGER,
-                    CONSTRAINT someone UNIQUE (user_id, peer_id) ON CONFLICT IGNORE
+                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE
                 );"""
 
 banned = """CREATE TABLE IF NOT EXISTS banned
                 (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    user_id INTEGER,
-                    user_name TEXT,
-                    user_url TEXT,
-                    banned_by_id INTEGER,
-                    banned_by_name TEXT,
-                    banned_by_url TEXT,
+                    peer_id INTEGER PRIMARY KEY REFERENCES conversations (peer_id) ON DELETE CASCADE,
+                    initiator_id INTEGER,
+                    initiator_name TEXT,
+                    target_id INTEGER,
+                    target_name TEXT,
                     ban_time INTEGER,
                     unban_time INTEGER,
-                    CONSTRAINT someone UNIQUE (user_id, peer_id) ON CONFLICT IGNORE  
+                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE  
                 );"""
 
 muted = """CREATE TABLE IF NOT EXISTS muted
                (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    user_id INTEGER,
-                    user_name TEXT,
-                    user_url TEXT,
-                    muted_by_id INTEGER,
-                    muted_by_name TEXT,
-                    muted_by_url TEXT,
+                    peer_id INTEGER PRIMARY KEY REFERENCES conversations (peer_id) ON DELETE CASCADE,
+                    initiator_id INTEGER,
+                    initiator_name TEXT,
+                    target_id INTEGER,
+                    target_name TEXT,
                     mute_time INTEGER,
-                    unmute_time INTEGER ,
-                    CONSTRAINT someone UNIQUE (user_id, peer_id) ON CONFLICT IGNORE 
+                    unmute_time INTEGER,
+                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE 
                );"""
 
 warned = """CREATE TABLE IF NOT EXISTS warned
                 (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    user_id INTEGER,
-                    user_name TEXT,
-                    user_url TEXT,
-                    warned_by_id INTEGER,
-                    warned_by_name TEXT,
-                    warned_by_url TEXT,
+                    peer_id INTEGER PRIMARY KEY REFERENCES conversations (peer_id) ON DELETE CASCADE,
+                    initiator_id INTEGER,
+                    initiator_name TEXT,
+                    target_id INTEGER,
+                    target_name TEXT,
+                    warn_count INTEGER,
                     warn_time INTEGER,
                     unwarn_time INTEGER,
-                    warn_count INTEGER,
-                    CONSTRAINT someone UNIQUE (user_id, peer_id) ON CONFLICT REPLACE 
+                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT REPLACE 
                 );"""
 
 queue = """CREATE TABLE IF NOT EXISTS queue
                 (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    user_id INTEGER,
-                    user_name TEXT,
-                    user_url TEXT,
+                    peer_id INTEGER PRIMARY KEY REFERENCES conversations (peer_id) ON DELETE CASCADE,
+                    initiator_id INTEGER,
+                    initiator_name TEXT,
+                    target_id INTEGER,
+                    target_name TEXT,
                     send_time INTEGER,
-                    next_send_time INTEGER,
-                    CONSTRAINT someone UNIQUE (user_id, peer_id) ON CONFLICT IGNORE
+                    next_time INTEGER,
+                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE
                 );"""
 
-tables = [conversations, settings, permissions, kicked, banned, warned, muted, queue]
+tables = [conversations, settings, permissions, queue, kicked, banned, muted, warned]
