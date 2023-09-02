82a2e4a15a6ed64262c6c7c8dcdb610a46a92e08
Oidaho <ruslan30403@gmail.com>
2023-09-03 06:37:26 +1000

    Добавлена ORM в прямое взаимодействие

--- handlers/abc.py
+++ handlers/abc.py
@@ -1,14 +1,16 @@
 from abc import ABC, abstractmethod
 from vkbottle import Bot
 from config import TOKEN
-from database import Processor
+from database.orm import DataBase
+from database.proc import Processor
 from utils import Info
-from utils.convertor import Converter
+from utils.converter import Converter
 
 
 class ABCHandler(ABC):
     def __init__(self):
         self.bot = Bot(token=TOKEN)
+        self.database = DataBase()
         self.processor = Processor()
         self.converter = Converter()
         self.info = Info()
