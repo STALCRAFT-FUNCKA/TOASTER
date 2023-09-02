bed7e53df078e23bba3a786ae3a90c748e8b4540
Oidaho <ruslan30403@gmail.com>
2023-09-03 06:37:08 +1000

    Фиксы

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,13 +3,15 @@ from .filters import *
 
 # !Сначала фильтры!, потом блок команд
 labelers = [
-    queue_filter.bl,
+    queue_filter.bl,            # Фильтры, зависящие от мута
     forbidden_filter.bl,
     curse_filter.bl,
-    url_filter.bl,
     age_filter.bl,
-    mute_filter.bl,
 
-    commands.bl,
+    mute_filter.bl,             # Фильтры мута
+
+    url_filter.bl,              # Фильтры, не зависящие от мута
+
+    commands.bl,                # Блок команд
     commands_mention.bl
 ]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,7 @@
-from .logger import Logger
-from .convertor import Converter
+from .converter import Converter
 from .informer import Info
 
 __all__ = (
-    "Logger",
     "Converter",
     "Info"
 )
