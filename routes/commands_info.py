3e285a0e93e8e611f60e792fe2920d8ac022f16c
Oidaho <ruslan30403@gmail.com>
2023-09-10 01:24:45 +1000

    FIX: Команда reference вынесена в блок информационных команд

--- routes/commands_info.py
+++ routes/commands_info.py
@@ -10,7 +10,43 @@ bl = BotLabeler()
 
 info = Info()
 converter = Converter()
-processor = InformationProcessor()
+info_processor = InformationProcessor()
+ref_processor = ReferenceProcessor()
+
+
+@bl.chat_message(
+    HandleCommand(ALIASES['reference'], PREFIXES, 1),
+    CollapseCommand(),
+    AnswerCommand(use_reply=False, use_fwd=False),
+    CheckPermission(access_to=PERMISSION_ACCESS['reference']),
+    HandleIn(handle_log=True, handle_chat=False)
+)
+async def reference(message: Message, args: Tuple):
+    context = {
+        "peer_id": message.peer_id,
+        "chat_id": message.chat_id,
+    }
+
+    if args[0] == "all":
+        await ref_processor.ref_all_proc(context)
+
+    if args[0] in ALIASES['reference']:
+        await ref_processor.info_reference_proc(context)
+
+    if args[0] in ALIASES['chat']:
+        await ref_processor.info_reference_proc(context)
+
+    if args[0] in ALIASES['log']:
+        await ref_processor.info_reference_proc(context)
+
+    if args[0] in ALIASES['drop']:
+        await ref_processor.info_reference_proc(context)
+
+    if args[0] in ALIASES['permission']:
+        await ref_processor.info_reference_proc(context)
+
+    if args[0] in ALIASES['settings']:
+        await ref_processor.info_reference_proc(context)
 
 
 @bl.chat_message(
