1678eb526795a98d0365e87389c8e18469ccc1d8
Oidaho <ruslan30403@gmail.com>
2023-09-11 02:56:53 +1000

    REFACTOR: Изменен общий принцип и подход к написанию

--- routes/commands_info.py
+++ routes/commands_info.py
@@ -26,65 +26,31 @@ async def reference(message: Message, args: Tuple):
         "chat_id": message.chat_id,
     }
 
-    if args[0] == "all":
-        await ref_processor.ref_all_proc(context)
+    args_check = [
+        (("all",), ref_processor.ref_all_proc(context)),
+        (ALIASES['reference'], ref_processor.ref_reference_proc(context)),
+        (ALIASES['chat'], ref_processor.ref_chat_proc(context)),
+        (ALIASES['log'], ref_processor.ref_log_proc(context)),
+        (ALIASES['drop'], ref_processor.ref_drop_proc(context)),
+        (ALIASES['setting'], ref_processor.ref_setting_proc(context)),
+        (ALIASES['delete'], ref_processor.ref_delete_proc(context)),
+        (ALIASES['copy'], ref_processor.ref_copy_proc(context)),
+        (ALIASES['terminate'], ref_processor.ref_terminate_proc(context)),
+        (ALIASES['kick'], ref_processor.ref_kick_proc(context)),
+        (ALIASES['ban'], ref_processor.ref_ban_proc(context)),
+        (ALIASES['unban'], ref_processor.ref_unban_proc(context)),
+        (ALIASES['mute'], ref_processor.ref_mute_proc(context)),
+        (ALIASES['unmute'], ref_processor.ref_unmute_proc(context)),
+        (ALIASES['warn'], ref_processor.ref_warn_proc(context)),
+        (ALIASES['unwarn'], ref_processor.ref_unwarn_proc(context)),
+        (ALIASES['queue'], ref_processor.ref_queue_proc(context)),
+        (ALIASES['unqueue'], ref_processor.ref_unqueue_proc(context)),
+        (ALIASES['info'], ref_processor.ref_info_proc(context))
+    ]
 
-    if args[0] in ALIASES['reference']:
-        await ref_processor.ref_reference_proc(context)
-
-    if args[0] in ALIASES['chat']:
-        await ref_processor.ref_chat_proc(context)
-
-    if args[0] in ALIASES['log']:
-        await ref_processor.ref_log_proc(context)
-
-    if args[0] in ALIASES['drop']:
-        await ref_processor.ref_log_proc(context)
-
-    if args[0] in ALIASES['permission']:
-        await ref_processor.ref_permission_proc(context)
-
-    if args[0] in ALIASES['setting']:
-        await ref_processor.ref_setting_proc(context)
-
-    if args[0] in ALIASES['delete']:
-        await ref_processor.ref_delete_proc(context)
-
-    if args[0] in ALIASES['copy']:
-        await ref_processor.ref_copy_proc(context)
-
-    if args[0] in ALIASES['terminate']:
-        await ref_processor.ref_terminate_proc(context)
-
-    if args[0] in ALIASES['kick']:
-        await ref_processor.ref_kick_proc(context)
-
-    if args[0] in ALIASES['ban']:
-        await ref_processor.ref_ban_proc(context)
-
-    if args[0] in ALIASES['unban']:
-        await ref_processor.ref_unban_proc(context)
-
-    if args[0] in ALIASES['mute']:
-        await ref_processor.ref_mute_proc(context)
-
-    if args[0] in ALIASES['unmute']:
-        await ref_processor.ref_unmute_proc(context)
-
-    if args[0] in ALIASES['warn']:
-        await ref_processor.ref_warn_proc(context)
-
-    if args[0] in ALIASES['unwarn']:
-        await ref_processor.ref_unwarn_proc(context)
-
-    if args[0] in ALIASES['queue']:
-        await ref_processor.ref_queue_proc(context)
-
-    if args[0] in ALIASES['unqueue']:
-        await ref_processor.ref_unqueue_proc(context)
-
-    if args[0] in ALIASES['info']:
-        await ref_processor.ref_info_proc(context)
+    for name, proc in args_check:
+        if args[0] in name:
+            await proc
 
 
 @bl.chat_message(
@@ -100,23 +66,16 @@ async def info(message: Message, args: Tuple):
         "chat_id": message.chat_id
     }
 
-    if args[0] in ALIASES['permission']:
-        await info_processor.info_permission_proc(context)
+    args_check = [
+        (ALIASES['permission'], info_processor.info_permission_proc(context)),
+        (ALIASES['setting'], info_processor.info_setting_proc(context)),
+        (ALIASES['chat'], info_processor.info_chat_proc(context)),
+        (ALIASES['kick'], info_processor.info_kick_proc(context)),
+        (ALIASES['ban'], info_processor.info_ban_proc(context)),
+        (ALIASES['mute'], info_processor.info_mute_proc(context)),
+        (ALIASES['warn'], info_processor.info_warn_proc(context)),
+    ]
 
-    if args[0] in ALIASES['setting']:
-        await info_processor.info_setting_proc(context)
-
-    if args[0] in ALIASES['chat']:
-        await info_processor.info_chat_proc(context)
-
-    if args[0] in ALIASES['kick']:
-        await info_processor.info_kick_proc(context)
-
-    if args[0] in ALIASES['ban']:
-        await info_processor.info_ban_proc(context)
-
-    if args[0] in ALIASES['mute']:
-        await info_processor.info_mute_proc(context)
-
-    if args[0] in ALIASES['warn']:
-        await info_processor.info_warn_proc(context)
+    for name, proc in args_check:
+        if args[0] in name:
+            await proc
