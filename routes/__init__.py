2fd795aaacb22c7e305158f15360483017ea4f19
Oidaho <76991612+Oidaho@users.noreply.github.com>
2023-09-20 21:35:29 +1000

    FIX: Я уже в тильте

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,7 +3,25 @@ from .filters import *
 from .handlers import *
 
 labelers = [
+    queue_filter,
+    forbidden_filter,
+    curse_filter,
+    age_filter,
+    mute_filter,
+    url_filter,
 
+    info_command,
+    reference_command,
+    ban_command,
+    copy_command,
+    delete_command,
+    kick_command,
+    mark_command,
+    mute_command,
+    permission_command,
+    queue_command,
+    setting_command,
+    warn_command
 ]
 
 handlers = [
