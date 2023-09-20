24aa89678886974b71d7000d278c095c80606c01
Oidaho <76991612+Oidaho@users.noreply.github.com>
2023-09-20 13:11:57 +1000

    FIX: Изменен синтаксис таблиц под MySQL

--- app/data/core.py
+++ app/data/core.py
@@ -1,91 +1,129 @@
-conversations = """CREATE TABLE IF NOT EXISTS conversations
-               (
-                   id INTEGER PRIMARY KEY,
-                   peer_id INTEGER UNIQUE ON CONFLICT REPLACE,
-                   peer_name TEXT,
-                   peer_type TEXT
-               );"""
+conversations = """
+CREATE TABLE IF NOT EXISTS conversations
+    (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
 
-permissions = """CREATE TABLE IF NOT EXISTS permissions
-                (
-                    id INTEGER PRIMARY KEY,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    target_id INTEGER, 
-                    target_name TEXT,
-                    target_lvl INTEGER,
-                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT REPLACE
-                );"""
+        peer_id INTEGER UNIQUE,
+        peer_name VARCHAR(255),
+        peer_type VARCHAR(255)
+    );
+"""
 
-settings = """CREATE TABLE IF NOT EXISTS settings
-                (
-                    id INTEGER PRIMARY KEY,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    setting_name TEXT,
-                    setting_status INTEGER,
-                    CONSTRAINT someone UNIQUE (peer_id, setting_name) ON CONFLICT IGNORE
-                );"""
+permissions = """
+CREATE TABLE IF NOT EXISTS permissions
+    (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
 
-kicked = """CREATE TABLE IF NOT EXISTS kicked
-                (
-                    id INTEGER PRIMARY KEY,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    initiator_id INTEGER,
-                    initiator_name TEXT,
-                    target_id INTEGER,
-                    target_name TEXT,
-                    kick_time INTEGER,
-                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE
-                );"""
+        peer_id INTEGER,
+        target_id INTEGER, 
+        target_name VARCHAR(255),
+        target_lvl INTEGER,
 
-banned = """CREATE TABLE IF NOT EXISTS banned
-                (
-                    id INTEGER PRIMARY KEY,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    initiator_id INTEGER,
-                    initiator_name TEXT,
-                    target_id INTEGER,
-                    target_name TEXT,
-                    ban_time INTEGER,
-                    unban_time INTEGER,
-                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE  
-                );"""
+        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
+        CONSTRAINT permission UNIQUE (peer_id, target_id)
+    );
+"""
 
-muted = """CREATE TABLE IF NOT EXISTS muted
-               (
-                    id INTEGER PRIMARY KEY,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    initiator_id INTEGER,
-                    initiator_name TEXT,
-                    target_id INTEGER,
-                    target_name TEXT,
-                    mute_time INTEGER,
-                    unmute_time INTEGER,
-                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE 
-               );"""
+settings = """
+CREATE TABLE IF NOT EXISTS settings
+    (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
 
-warned = """CREATE TABLE IF NOT EXISTS warned
-                (
-                    id INTEGER PRIMARY KEY,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    initiator_id INTEGER,
-                    initiator_name TEXT,
-                    target_id INTEGER,
-                    target_name TEXT,
-                    warn_count INTEGER,
-                    warn_time INTEGER,
-                    unwarn_time INTEGER,
-                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT REPLACE 
-                );"""
+        peer_id INTEGER,
+        setting_name VARCHAR(255),
+        setting_status BOOLEAN,
 
-queue = """CREATE TABLE IF NOT EXISTS queue
-                (
-                    id INTEGER PRIMARY KEY,
-                    peer_id INTEGER REFERENCES conversations (peer_id) ON DELETE CASCADE,
-                    target_id INTEGER,
-                    target_name TEXT,
-                    send_time INTEGER,
-                    next_time INTEGER,
-                    CONSTRAINT someone UNIQUE (peer_id, target_id) ON CONFLICT IGNORE
-                );"""
+        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
+        CONSTRAINT setting UNIQUE (peer_id, setting_name)
+    );
+"""
 
-tables = [conversations, settings, permissions, queue, kicked, banned, muted, warned]
+kicked = """
+CREATE TABLE IF NOT EXISTS kicked
+    (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+        peer_id INTEGER,
+        initiator_id INTEGER,
+        initiator_name VARCHAR(255),
+        target_id INTEGER,
+        target_name VARCHAR(255),
+        kick_time INTEGER,
+
+        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
+        CONSTRAINT kick UNIQUE (peer_id, target_id)
+    );
+"""
+
+banned = """
+CREATE TABLE IF NOT EXISTS banned
+    (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+        peer_id INTEGER,
+        initiator_id INTEGER,
+        initiator_name VARCHAR(255),
+        target_id INTEGER,
+        target_name VARCHAR(255),
+        ban_time INTEGER,
+        unban_time INTEGER,
+
+        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
+        CONSTRAINT ban UNIQUE (peer_id, target_id) 
+    );
+"""
+
+muted = """
+CREATE TABLE IF NOT EXISTS muted
+   (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+        peer_id INTEGER,
+        initiator_id INTEGER,
+        initiator_name VARCHAR(255),
+        target_id INTEGER,
+        target_name VARCHAR(255),
+        mute_time INTEGER,
+        unmute_time INTEGER,
+
+        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
+        CONSTRAINT mute UNIQUE (peer_id, target_id)
+   );
+"""
+
+warned = """
+CREATE TABLE IF NOT EXISTS warned
+    (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+        peer_id INTEGER,
+        initiator_id INTEGER,
+        initiator_name VARCHAR(255),
+        target_id INTEGER,
+        target_name VARCHAR(255),
+        warn_count INTEGER,
+        warn_time INTEGER,
+        unwarn_time INTEGER,
+
+        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
+        CONSTRAINT warn UNIQUE (peer_id, target_id) 
+    );
+"""
+
+queue = """
+CREATE TABLE IF NOT EXISTS queue
+    (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+
+        peer_id INTEGER,
+        target_id INTEGER,
+        target_name VARCHAR(255),
+        send_time INTEGER,
+        next_time INTEGER,
+
+        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
+        CONSTRAINT message UNIQUE (peer_id, target_id)
+    );
+"""
+
+tables = [conversations, permissions, settings, kicked, banned, muted, warned, queue]
