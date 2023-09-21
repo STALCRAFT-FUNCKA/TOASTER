6e4764f3fb3c059c03faf81d2485136bec828e0d
Oidaho <76991612+Oidaho@users.noreply.github.com>
2023-09-22 01:22:23 +1000

    UPDATE: Оптимизация импорта

--- routes/commands/__init__.py
+++ routes/commands/__init__.py
@@ -1,16 +1,17 @@
 from .chat.information.info import bl as info_command
 from .chat.information.reference import bl as reference_command
 from .chat.moderation.ban import bl as ban_command
-from .chat.moderation.copy import bl as copy_command
-from .chat.moderation.delete import bl as delete_command
 from .chat.moderation.kick import bl as kick_command
-from .chat.moderation.mark import bl as mark_command
+from .chat.system.mark import bl as mark_command
 from .chat.moderation.mute import bl as mute_command
-from .chat.moderation.permission import bl as permission_command
+from .chat.system.permission import bl as permission_command
 from .chat.moderation.queue import bl as queue_command
-from .chat.moderation.setting import bl as setting_command
+from routes.commands.chat.system.setting import bl as setting_command
 from .chat.moderation.warn import bl as warn_command
 from .chat.fun.roll import bl as roll_command
+from .chat.fun.copy import bl as copy_command
+from .chat.fun.delete import bl as delete_command
+from .chat.fun.say import bl as say_command
 
 __all__ = (
     "info_command",
