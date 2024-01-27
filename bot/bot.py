88e12b2960db77e0200591fdcc572167c460691d
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-01-27 10:34:29 +1000

    UPDATE: Docstring

--- bot/bot.py
+++ bot/bot.py
@@ -2,34 +2,44 @@ import os
 from vk_api import VkApi
 from vk_api.longpoll import VkLongPoll
 
-
 class Bot(object):
-    __session = None
-    __longpoll = None
+    """
+    Bot main class
+    """
+    _session = None
+    _longpoll = None
 
     api = None
 
-    def __create_session(self):
-        self.__session = VkApi(
+    def _create_session(self):
+        self._session = VkApi(
             token=os.getenv("TOASTER_DEV_TOKEN"),
             api_version="5.199"
         )
 
-    def __create_longpoll(self):
-        self.__longpoll = VkLongPoll(
-            vk=self.__session,
+    def _create_longpoll(self):
+        self._longpoll = VkLongPoll(
+            vk=self._session,
             wait=10,
             group_id=os.getenv("TOASTER_DEV_GROUPID")
         )
 
-    def __create_api(self):
-        self.api = self.__session.get_api()
+    def _create_api(self):
+        self.api = self._session.get_api()
+
+    def _fabricate_event(self, raw_event):
+        # TODO: write me
+        return raw_event
 
     def __init__(self):
-        self.__create_session()
-        self.__create_longpoll()
-        self.__create_api()
+        self._create_session()
+        self._create_longpoll()
+        self._create_api()
 
     def run(self):
-        for event in self.__longpoll.listen():
+        """
+        Starts listening VK long polling server.
+        """
+        for raw_event in self._longpoll.listen():
+            event = self._fabricate_event(raw_event)
             print(event)
