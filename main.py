00e3f3a2b2a51edbbfd97f5ba074bcdcd18f07ca
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-01-28 21:25:40 +1000

    ADD: Logger init

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import logging
 from bot import Bot
 
+logger = logging.getLogger("TOASTER")
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(name)s | %(levelname)s | %(asctime)s] Message: %(message)s")
+stream_handler.setFormatter(formatter)
+
+logger.setLevel(logging.INFO)
+logger.addHandler(stream_handler)
+
+logger.info("Log messages powered by \"logging\" module.")
 
 TOASTER = Bot()
 
