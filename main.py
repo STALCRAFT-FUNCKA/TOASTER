f18c538553f969945f66568efefb89aa78788406
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-01-29 22:21:49 +1000

    UPDATE: Logger and docstrings

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+"""
+Main app file.
+"""
 import logging
+from colorama import Fore
 from bot import Bot
 
+# Initializing logger
 logger = logging.getLogger("TOASTER")
 
 stream_handler = logging.StreamHandler()
-formatter = logging.Formatter("[%(name)s | %(levelname)s | %(asctime)s] Message: %(message)s")
+formatter = logging.Formatter(
+    Fore.RED + "[%(name)s | %(levelname)s | %(asctime)s] " + Fore.WHITE +"Message: %(message)s"
+)
 stream_handler.setFormatter(formatter)
 
 logger.setLevel(logging.INFO)
@@ -12,6 +19,7 @@ logger.addHandler(stream_handler)
 
 logger.info("Log messages powered by \"logging\" module.")
 
+# Initializing bot (TOASTER)
 TOASTER = Bot()
 
 if __name__ == "__main__":
