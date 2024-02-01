6431b7713103b253e149920be572b2daa5a79cbc
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-02-01 20:13:05 +1000

    UPDATE: Test keyboard

--- bot/handlers/command_handlers/handlers.py
+++ bot/handlers/command_handlers/handlers.py
@@ -1,3 +1,4 @@
+import json
 from vk_api import VkApi
 from ..base_handlers import ABCHandler
 from ...event_factory import MessageEvent
@@ -17,12 +18,42 @@ class TestCommand(ABCHandler):
         answer_text = f"Вызвана комманда <{self.COMMAND_NAME}> " \
                       f"с аргументами {kwargs.get('argument_list')}."
 
+        answer_keyboard = {
+            "inline": True,
+            "buttons": [
+                [
+                    {
+                        "action":{
+                            "type": "callback",
+                            "payload": {
+                                "keyboard_name": "test",
+                                "button": "Test1"
+                            },
+                            "label": "Test1",
+                        },
+                        "color": "positive"
+                    },
+                    {
+                        "action":{
+                            "type": "callback",
+                            "payload": {
+                                "keyboard_name": "test",
+                                "button": "Test2"
+                            },
+                            "label": "Test2",
+                        },
+                        "color": "negative"
+                    },
+                ]
+            ]
+        }
         api.messages.send(
             peer_id=event.peer_id,
             random_id=0,
-            message=answer_text
+            message=answer_text,
+            keyboard=json.dumps(answer_keyboard)
         )
-        
+
         return True
 
 
