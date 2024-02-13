7b437e65d77a3677b39808af5948baee1991607b
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-02-13 15:30:07 +1000

    ADD: Singletone meta-class pattern

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,9 @@
 """A module of tools designed to quickly
 and convenient creation of VK API objects and bot systems.
 """
+from .singletone import MetaSingleton
+
+
+__all__ = (
+    "MetaSingleton",
+)
---
+++ tools/singletone.py
@@ -0,0 +1,15 @@
+"""File with singletone meta-class.
+"""
+
+class MetaSingleton(type):
+    """Meta-class implementing the singleton pattern.
+
+    Returns:
+        cls: Class object instance.
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
