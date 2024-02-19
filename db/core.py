99adf68809e5115be68baad61e5e2c39039621d2
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-02-19 23:54:42 +1000

    UPDATE: Removed usless functions

--- db/core.py
+++ db/core.py
@@ -1,127 +1,8 @@
 """
 This file describes the main queries that form the structure of the database.
 """
+# TODO: add new Tables
 
-# Table
-CONVERSATIONS = """
-CREATE TABLE IF NOT EXISTS conversations
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-        peer_id INTEGER UNIQUE,
-        peer_name VARCHAR(255),
-        peer_type VARCHAR(255)
-    );
-"""
-
-# Table
-PERMISSIONS = """
-CREATE TABLE IF NOT EXISTS permissions
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-        peer_id INTEGER,
-        target_id INTEGER, 
-        target_name VARCHAR(255),
-        target_lvl INTEGER,
-
-        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
-        CONSTRAINT permission UNIQUE (peer_id, target_id)
-    );
-"""
-
-# Table
-SETTINGS = """
-CREATE TABLE IF NOT EXISTS settings
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-        peer_id INTEGER,
-        setting_name VARCHAR(255),
-        setting_status BOOLEAN,
-
-        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
-        CONSTRAINT setting UNIQUE (peer_id, setting_name)
-    );
-"""
-
-# Table
-KICKED = """
-CREATE TABLE IF NOT EXISTS kicked
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-        peer_id INTEGER,
-        initiator_id INTEGER,
-        initiator_name VARCHAR(255),
-        target_id INTEGER,
-        target_name VARCHAR(255),
-        kick_time INTEGER,
-
-        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
-        CONSTRAINT kick UNIQUE (peer_id, target_id)
-    );
-"""
-
-# Table
-BANNED = """
-CREATE TABLE IF NOT EXISTS banned
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-        peer_id INTEGER,
-        initiator_id INTEGER,
-        initiator_name VARCHAR(255),
-        target_id INTEGER,
-        target_name VARCHAR(255),
-        ban_time INTEGER,
-        unban_time INTEGER,
-
-        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
-        CONSTRAINT ban UNIQUE (peer_id, target_id) 
-    );
-"""
-
-# Table
-MUTED = """
-CREATE TABLE IF NOT EXISTS muted
-   (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-        peer_id INTEGER,
-        initiator_id INTEGER,
-        initiator_name VARCHAR(255),
-        target_id INTEGER,
-        target_name VARCHAR(255),
-        mute_time INTEGER,
-        unmute_time INTEGER,
-
-        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
-        CONSTRAINT mute UNIQUE (peer_id, target_id)
-   );
-"""
-
-# Table
-WARNED = """
-CREATE TABLE IF NOT EXISTS warned
-    (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-        peer_id INTEGER,
-        initiator_id INTEGER,
-        initiator_name VARCHAR(255),
-        target_id INTEGER,
-        target_name VARCHAR(255),
-        warn_count INTEGER,
-        warn_time INTEGER,
-        unwarn_time INTEGER,
-
-        FOREIGN KEY (peer_id) REFERENCES conversations(peer_id) ON DELETE CASCADE,
-        CONSTRAINT warn UNIQUE (peer_id, target_id) 
-    );
-"""
-
-# Table
 QUEUE = """
 CREATE TABLE IF NOT EXISTS queue
     (
@@ -138,5 +19,5 @@ CREATE TABLE IF NOT EXISTS queue
     );
 """
 
-# Table
-tables = [CONVERSATIONS, PERMISSIONS, SETTINGS, KICKED, BANNED, MUTED, WARNED, QUEUE]
+# Tables
+tables = []
--- db/orm.py
+++ db/orm.py
@@ -3,7 +3,6 @@ This file contains a database connection class and
 an object relational model for interacting with it.
 """
 import os
-import logging
 import MySQLdb
 from tools import MetaSingleton
 from .core import tables
@@ -13,22 +12,6 @@ class Connection:
     """This class provides connection
     to the MySQL database.
     """
-    def _fill_schema(self):
-        """Creates a database named "toaster"
-        if one does not exist.
-        """
-        self._cursor.execute('CREATE DATABASE IF NOT EXISTS toaster')
-
-
-    def _fill_tables(self):
-        """Populates the database with
-        a standard table structure.
-        """
-        self._cursor.execute('USE toaster')
-        for table in tables:
-            self._cursor.execute(table)
-
-
     def __init__(self, allow_debug_text=True):
         try:
             self._connection = MySQLdb.connect(
@@ -51,13 +34,31 @@ class Connection:
                 print("Ошибка при подключении к MySQL Server", error)
 
 
-    def get_cursor(self):
+    def _fill_schema(self):
+        """Creates a schema named "toaster"
+        if one does not exist.
+        """
+        self._cursor.execute('CREATE DATABASE IF NOT EXISTS toaster')
+
+
+    def _fill_tables(self):
+        """Populates the database with
+        a standard table structure.
+        """
+        self._cursor.execute('USE toaster')
+        for table in tables:
+            self._cursor.execute(table)
+
+
+    @property
+    def cursor(self):
         """Returns database cursor object.
         """
         return self._cursor
 
 
-    def get_connection(self):
+    @property
+    def connection(self):
         """Returns database connection object.
         """
         return self._connection
@@ -68,184 +69,25 @@ class BaseTable:
     """Defines the base object of the table in object-relative
     representation and the main methods of interaction with it.
     """
-    _ops = {
-        '__le': '<=',
-        '__lt': '<',
-        '__ge': '>=',
-        '__gt': '>',
-        '__nt': '!='
-    }
-
-    def _get_ratio(self, rows: dict) -> list:
-        """When specifying a method for comparing variables in an ORM query method,
-        you must use keywords. Key characters are transformed by this
-        method into comparison operators. The function returns a list
-        of strings that are arranged according to the desired pattern.
-
-        Args:
-            rows (dict): rows with transformation keys into comparison operators
-
-        Returns:
-            list: list of args with sql somprassion operators.
-        """
-        summary = []
-        for key, value in rows.items():
-            op = key[-4:-1]+key[-1]
-            op = self._ops.get(op, '=')
-
-            if op != '=':
-                key = key[0:-4]
-
-            summary.append(f"{key} {op} '{value}'")
-
-        return summary
-
-
     def __init__(self, table_name, connection, cursor):
         self.table_name = table_name
         self.con = connection
         self.cur = cursor
 
 
-    def select(self, fields: tuple = None, **rows):
-        """Accepts arguments for fields, comparisons, etc., 
-        forms a database select query from them and returns the result of its execution.
-        Keys that mimic comparison operators:
-            1) __le -> <= \n
-            2) __lt -> <  \n
-            3) __ge -> >= \n
-            4) __gt -> >  \n
-            5) __nt -> != \n
-
-        Example rows:
-            id__lt=10 -> id<10
+    def raw(self, query: str):
+        """Executing a raw query.
 
         Args:
-            fields (tuple, optional): Fields for which it is necessary to obtain data
-            in the database. Defaults to None.
+            query (str): Raw SQL query.
 
         Returns:
-            str: MySQL query string.
+            tuple: Tuple of rows.
         """
-        if fields:
-            summary_fields = ', '.join(fields)
-        else:
-            summary_fields = '*'
-
-        query = f"SELECT {summary_fields} FROM {self.table_name}"
-
-        if rows:
-            summary_rows = ' AND '.join(self._get_ratio(rows))
-            query += f" WHERE {summary_rows}"
-
-        query += ";"
-
         self.cur.execute('USE toaster;')
         self.cur.execute(query)
         result = self.cur.fetchall()
-        logging.debug("Query: %s", query)
-        logging.debug("Result: %s", result)
-        return result
-
-
-    def insert(self, on_duplicate=None, **rows):
-        """Takes arguments for fields, comparisons, etc., 
-        forms a database insert query from them and inserts data when it is executed.
-
-        Args:
-            on_duplicate (str, optional): On duplicate action. 
-            Can be "ignore" or "update". Defaults to None.
-        """
-        if not rows:
-            return
-
-        summary_keys = ', '.join(rows.keys())  # Might be incorrect
-        summary_values = ', '.join([f"'{value}'" for value in rows.values()])
-        query = f""" INSERT INTO {self.table_name} ({summary_keys})
-                     VALUES ({summary_values})
-                """
-
-        if on_duplicate == "ignore":
-            query += " ON DUPLICATE KEY UPDATE id=id"
-
-        if on_duplicate == "update":
-            query += f""" ON DUPLICATE KEY UPDATE {', '.join(
-                            [f"{key}='{value}'" for key, value in rows.items()]
-                        )}
-                     """
-
-        query += ";"
-
-        self.cur.execute('USE toaster;')
-        self.cur.execute(query)
-        logging.debug("Query: %s", query)
-        logging.debug("Result: executed")
-
-
-    def update(self, new_data: dict, **rows):
-        """Accepts arguments for fields, comparisons, etc.,
-        forms a query from them to update the database
-        and updates the data according to the dictionary
-        of correspondences received as input when executing the request.
-        Keys that mimic comparison operators:
-            1) __le -> <= \n
-            2) __lt -> <  \n
-            3) __ge -> >= \n
-            4) __gt -> >  \n
-            5) __nt -> != \n
-
-        Example rows:
-            id__lt=10 -> id<10
-
-        Args:
-            new_data (dict): Dict of correspondences for replacing variable values.
-        """
-        if not new_data:
-            return
-
-        summary_fields = ', '.join(
-            [f"{key}='{value}'" for key, value in new_data.items()])
-        query = f"UPDATE {self.table_name} SET {summary_fields}"
-
-        if rows:
-            summary_rows = ' AND '.join(self._get_ratio(rows))
-            query += f" WHERE {summary_rows}"
-
-        query += ";"
-
-        self.cur.execute('USE toaster;')
-        self.cur.execute(query)
-        logging.debug("Query: %s", query)
-        logging.debug("Result: executed")
-
-
-    def delete(self, **rows):
-        """Takes arguments for fields, comparisons, etc.,
-        forms a request from them to delete from the
-        database and deletes data according to the
-        conditions specified by the comparison operators.
-        Keys that mimic comparison operators:
-            1) __le -> <= \n
-            2) __lt -> <  \n
-            3) __ge -> >= \n
-            4) __gt -> >  \n
-            5) __nt -> != \n
-
-        Example rows:
-            id__lt=10 -> id<10
-        """
-        query = f"DELETE FROM {self.table_name}"
-
-        if rows:
-            summary_rows = ' AND '.join(self._get_ratio(rows))
-            query += f" WHERE {summary_rows}"
-
-        query += ";"
-
-        self.cur.execute('USE toaster;')
-        self.cur.execute(query)
-        logging.debug("Query: %s", query)
-        logging.debug("Result: executed")
+        return result if result else None
 
 
 
@@ -258,97 +100,14 @@ class DataBase(metaclass=MetaSingleton):
     _base_table = BaseTable
     _tunnel = Connection()
 
+    # TODO: add normal tables later
     @property
-    def conversations(self):
+    def table(self):
         """A table property that implements
         object-relational access to the conversion table.
         """
         return self._base_table(
-            table_name="conversations",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
-        )
-
-
-    @property
-    def settings(self):
-        """A table property that implements
-        object-relational access to the settings table.
-        """
-        return self._base_table(
-            table_name="settings",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
-        )
-
-
-    @property
-    def permissions(self):
-        """A table property that implements
-        object-relational access to the permissions table.
-        """
-        return self._base_table(
-            table_name="permissions",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
-        )
-
-
-    @property
-    def kicked(self):
-        """A table property that implements
-        object-relational access to the kicked table.
-        """
-        return self._base_table(
-            table_name="kicked",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
-        )
-
-
-    @property
-    def banned(self):
-        """A table property that implements
-        object-relational access to the banned table.
-        """
-        return self._base_table(
-            table_name="banned",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
-        )
-
-
-    @property
-    def warned(self):
-        """A table property that implements
-        object-relational access to the warned table.
-        """
-        return self._base_table(
-            table_name="warned",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
-        )
-
-
-    @property
-    def muted(self):
-        """A table property that implements
-        object-relational access to the muted table.
-        """
-        return self._base_table(
-            table_name="muted",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
-        )
-
-
-    @property
-    def queue(self):
-        """A table property that implements
-        object-relational access to the queue table.
-        """
-        return self._base_table(
-            table_name="queue",
-            connection=self._tunnel.get_connection(),
-            cursor=self._tunnel.get_cursor()
+            table_name="table",
+            connection=self._tunnel.connection,
+            cursor=self._tunnel.cursor
         )
