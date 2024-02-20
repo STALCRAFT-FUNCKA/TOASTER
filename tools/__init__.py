79ce2a26efc9f84992f2db4096d34b6a69d2cecb
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-02-20 22:13:57 +1000

    ADD: Datetime tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,8 +2,16 @@
 and convenient creation of VK API objects and bot systems.
 """
 from .singletone import MetaSingleton
+from .time import (
+    timestamp,
+    msk_now,
+    msk_delta
+)
 
 
 __all__ = (
     "MetaSingleton",
+    "timestamp",
+    "msk_now",
+    "msk_delta"
 )
