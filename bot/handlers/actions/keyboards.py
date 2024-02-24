70d6d75bf65eb75d31c67de680e23d3254f22d4c
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-02-24 14:40:58 +1000

    ADD: Keyboards to swap

---
+++ bot/handlers/actions/keyboards.py
@@ -0,0 +1,11 @@
+"""_summary_
+"""
+from tools.keyboard import (
+    Keyboard,
+)
+
+
+# -------------------------------------------------------------------
+EmptyKbd = (
+    Keyboard(inline=True, one_time=False)
+)
