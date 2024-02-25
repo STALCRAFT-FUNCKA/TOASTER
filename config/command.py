7b8d732f6fc4c5c70fe634e61441107a96ba2c09
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-02-25 19:15:30 +1000

    Permissions decoding

--- config/command.py
+++ config/command.py
@@ -5,6 +5,12 @@ COMMAND_PREFIXES: tuple = (
 
 MAX_COMMAND_ARG_COUNT: int = 10
 
+PERMISSIONS_DECODING = {
+    0: "Пользователь",
+    1: "Модератор",
+    2: "Администратор"
+}
+
 COMMAND_PERMISSIONS = {
     "test": 2,
     "mark": 2,
